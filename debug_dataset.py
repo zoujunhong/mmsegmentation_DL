014dfde1370b270d8b56855459a4b3c78cfb1a36
lilmortyj <781113402@qq.com>
2023-04-23 14:54:46 +0000

    stuffs

--- debug_dataset.py
+++ debug_dataset.py
@@ -7,12 +7,19 @@ import cv2
 
 
 def show_one_sample(sample):
-    imgs = sample["inputs"]
-    seg_maps = sample["data_samples"].gt_sem_seg.data
-    for img, seg_map in zip(imgs, seg_maps):
-        cv2.imshow("Image", img.permute(1,2,0).numpy())
-        cv2.imshow("Segmentation", seg_map.numpy())
-        cv2.waitKey(0)
+    # print(sample.keys())
+    # print(sample)
+    # print()
+    print(sample["inputs"].shape)
+    print(sample["data_samples"].gt_sem_seg.data.shape)
+    print(sample["data_samples"].metainfo)
+    # print(sample["data_samples"])
+    # imgs = sample["inputs"]
+    # seg_maps = sample["data_samples"].gt_sem_seg.data
+    # for img, seg_map in zip(imgs, seg_maps):
+    #     cv2.imshow("Image", img.permute(1,2,0).numpy())
+    #     cv2.imshow("Segmentation", seg_map.numpy())
+    #     cv2.waitKey(0)
 
 train_dataset = VSPWDataset(data_root="./VSPW_480p", ann_file="train.txt", seq_length=5)
 train_pipeline = [dict(type="LoadVideoSequenceFromFile", resize=(640, 480))]
@@ -24,6 +31,8 @@ val_pipeline = [dict(type="LoadVideoSequenceFromFile", resize=(640, 480))]
 # test pipline
 loader = LoadVideoSequenceFromFile(flip_prob=0.5)
 
+# print(len(train_dataset))
+# print(len(val_dataset))
 # visualize data
 for sample in train_dataset:
     # load sample via pipeline
